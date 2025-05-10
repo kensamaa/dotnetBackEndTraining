cfcb6511ee0c34504ce5ed999c4f747a1125a7ea
kensamaa <amineab203@gmail.com>
2025-05-10 23:34:33 +0100

    add migration

---
+++ Infrastructure/Data/Migrations/20250510223253_InitialCreate.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    FirstName = table.Column<string>(type: "TEXT", nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", nullable: false),
+                    EnrollmentDate = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Students");
+        }
+    }
+}
--- infos.md
+++ infos.md
@@ -1,9 +1,12 @@
 ## life cycle
+
 Singleton: one instance for app lifetime
 
 Scoped: one instance per HTTP request
 
 Transient: new instance each injection
+
+### infos
 
 Transient: new StudentService each injection. Good when lightweight.
 
@@ -18,3 +21,13 @@ IEnumerable vs IQueryable: IQueryable translates to SQL, defer execution. IEnume
 AsNoTracking: use for read-only queries to improve performance.
 
 LINQ & EF: write queries against DbSet<T>, filter, project, and materialize asynchronously.
+
+## add migration
+
+Add a new migration
+cd Api
+dotnet ef migrations add InitialCreate --project ../Infrastructure/Infrastructure.csproj --startup-project Api.csproj --output-dir Data/Migrations
+
+### 2. Apply the migration to create/update the database
+
+dotnet ef database update --project ../Infrastructure/Infrastructure.csproj --startup-project Api.csproj
