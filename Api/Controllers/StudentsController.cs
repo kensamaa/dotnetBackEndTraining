eb3562f1bc0fc5e3348afa1be13cade0cba2ef27
kensamaa <amineab203@gmail.com>
2025-05-10 22:56:16 +0100

    addign the controller and apis

---
+++ Api/Controllers/StudentsController.cs
@@ -0,0 +1,36 @@
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class StudentsController : ControllerBase
+{
+    private readonly IMediator _mediator;
+    private readonly ILogger _logger;
+    public StudentsController(IMediator mediator, ILogger<StudentsController> logger)
+    {
+        _mediator = mediator;
+        _logger = logger;
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create([FromBody] CreateStudentCommand cmd)
+    {
+        var id = await _mediator.Send(cmd);
+        return CreatedAtAction(nameof(GetById), new { id }, null);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var student = await _mediator.Send(new GetStudentByIdQuery(id));
+        return student is null ? NotFound() : Ok(student);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var list = await _mediator.Send(new GetAllStudentsQuery());
+        return Ok(list);
+    }
+}
